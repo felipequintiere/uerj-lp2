b0a63b85ff2828d60c545ddda0b8f78886fcbd5d
Felipe Quintiere <felipequintiere@gmail.com>
2026-04-17 23:16:28 -0300

    update

--- 2.3.2pacotes/1.java
+++
@@ -1,5 +0,0 @@
-/*
-- Adapte as suas modificações anteriores (ex., cores de contorno e fundo) para estarem nas classes dividas em pacotes e com os modificadores de acesso apropriados.
-- Adicione um "print screen" da execução (imagem .png)
-- Mantenha essa estrutura em pacotes para todos os exercícios posteriores.
-*/
