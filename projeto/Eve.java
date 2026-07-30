932106a4cbe2499a37cf673ea00796b6b0091d3e
Felipe Quintiere <felipequintiere@gmail.com>
2026-07-30 17:07:49 -0300

    update

--- projeto/Eve.java
+++ projeto/Eve.java
@@ -2,6 +2,9 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import figures.*;
 
 public class Eve {
@@ -14,6 +17,9 @@ class MeuFrame extends JFrame {
 	Rect r1;
 	Ellipse e1;
 
+	ArrayList<Rect> rs = new ArrayList<Rect>();
+	Random rand = new Random();  // ADD THIS
+
 	public MeuFrame() {
 		this.addWindowListener (
 			new WindowAdapter() {
@@ -23,9 +29,25 @@ class MeuFrame extends JFrame {
 			}
 		);
 
-	this.setTitle("Eve - editor vetorial");
-	this.setSize(500, 500);
-	this.setVisible(true);
+		this.setTitle("Eve - Editor Vetorial");
+		this.setSize(500, 500);
+		this.setVisible(true);
+
+		// event listeners
+		this.addKeyListener(
+			new KeyAdapter() {
+				public void keyPressed(KeyEvent evt) {
+					if (evt.getKeyChar() == 'r') {
+						int x = rand.nextInt(w/2);
+						int y = rand.nextInt(w/2);
+						int w = rand.nextInt(w/2);
+						int h = rand.nextInt(w/2);
+						rs.add(new Rect(x,y, w,h));
+						repaint();
+					}
+				}
+			}
+		);
 	}
 
 	public void paint(Graphics g) {
